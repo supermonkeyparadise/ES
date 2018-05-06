63fdd31b7ec429acbc8711c16c7317bec4db1c4d
Unknown <steven.chou@uniqestate.com>
2018-05-06 16:18:38 +0800

    Booleans and Comparison Operators

---
+++ andrew_mead/the_modern_javaScript_bootcamp_2018/variables_and_flow_control/boolean_and_comparision_operators/boolean.js
@@ -0,0 +1,13 @@
+// ===
+// !==
+// <
+// >
+// <=
+// >=
+
+let age = 37;
+let isChild = age <= 7;
+let isSenior = age > 65;
+
+console.log(`isChild ${isChild}`);
+console.log(`isSenior ${isSenior}`);
