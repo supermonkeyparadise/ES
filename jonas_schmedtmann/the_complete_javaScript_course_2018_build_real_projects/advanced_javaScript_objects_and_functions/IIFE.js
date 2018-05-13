2215bac9afaa0f5a9b43fe9e06b6697c3c21bb0f
Unknown <steven.chou@uniqestate.com>
2018-05-13 14:40:40 +0800

    Immediately Invoked Function Expressions (IIFE)

---
+++ jonas_schmedtmann/the_complete_javaScript_course_2018_build_real_projects/advanced_javaScript_objects_and_functions/IIFE.js
@@ -0,0 +1,18 @@
+// function game() {
+//   const score = Math.random() * 10;
+//   console.log(score >= 5);
+// }
+
+// game();
+
+/**
+ *   1. Just for call once
+ *   2. 建立一個 new scope，所以變數不會衝突
+ *   3. [ Data Privacy ]
+ */
+(function(goodLuck) {
+  const score = Math.random() * 10;
+  console.log(score >= 5 - goodLuck);
+})(3);
+
+console.log('## score:', score);
