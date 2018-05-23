8fe203b484eaec86d58c1a71c0813cb7eb7ea834
Unknown <steven.chou@uniqestate.com>
2018-05-23 16:40:36 +0800

    Building the Search Controller

--- jonas_schmedtmann/the_complete_javaScript_course_2018_build_real_projects/using_es6_npm_babel_and_webpack/project_forkify/forkify_starter/src/js/index.js
+++ jonas_schmedtmann/the_complete_javaScript_course_2018_build_real_projects/using_es6_npm_babel_and_webpack/project_forkify/forkify_starter/src/js/index.js
@@ -1,5 +1,34 @@
 import Search from './models/Search';
+import $ from 'jquery';
 
-const search = new Search('pizza');
-console.log('## search obj:', search);
-search.getResult();
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Get query from view
+  const query = 'pizza';
+
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
+
+    // 3) Prepare UI for result
+
+    // 4) Search for recipe
+    // async function return Promise
+    await state.search.getResult();
+
+    // 5) render results on UI
+    console.log(state.search.result);
+  }
+};
+
+$('.search').on('submit', function(e) {
+  e.preventDefault();
+  controlSearch();
+});
