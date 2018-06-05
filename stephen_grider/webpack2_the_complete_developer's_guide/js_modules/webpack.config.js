1b2159e54c09ffc31e6cffc4694cb4de4fd19652
Unknown <steven.chou@uniqestate.com>
2018-06-05 16:10:00 +0800

    The Extract Text Plugin

--- stephen_grider/webpack2_the_complete_developer's_guide/js_modules/webpack.config.js
+++ stephen_grider/webpack2_the_complete_developer's_guide/js_modules/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
@@ -13,11 +14,18 @@ const config = {
         test: /\.js$/
       },
       {
-        use: ['style-loader', 'css-loader'],
+        // 將 CSS 整合在 bundle.js
+        // use: ['style-loader', 'css-loader'],
+
+        // 產生一個分開的 CSS file
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
         test: /\.css$/
       }
     ]
-  }
+  }, // 將所有由 css-loader 載入的 css，寫入到 style.css 檔案中
+  plugins: [new ExtractTextPlugin('style.css')]
 };
 
 module.exports = config;
