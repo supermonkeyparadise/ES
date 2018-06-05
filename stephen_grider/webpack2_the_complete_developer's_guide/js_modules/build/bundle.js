002ceefe857f4c45ff0dba0beef2a5fe6497cc2a
Unknown <steven.chou@uniqestate.com>
2018-06-05 14:23:43 +0800

    Babel Configuration

--- stephen_grider/webpack2_the_complete_developer's_guide/js_modules/build/bundle.js
+++ stephen_grider/webpack2_the_complete_developer's_guide/js_modules/build/bundle.js
@@ -68,22 +68,28 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-const sum = (a, b) => a + b;
+"use strict";
+
+
+var sum = function sum(a, b) {
+  return a + b;
+};
 
 module.exports = sum;
-
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-const sum = __webpack_require__(0);
+"use strict";
 
-const total = sum(1, 2);
+
+var sum = __webpack_require__(0);
+
+var total = sum(1, 2);
 console.log(total);
-
 
 /***/ }
 /******/ ]);
