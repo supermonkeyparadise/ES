aa2b3e4a7dca3fd760510e8128ba501531ce23c0
Unknown <steven.chou@uniqestate.com>
2018-06-05 16:46:46 +0800

    Handling Images with Webpack

--- stephen_grider/webpack2_the_complete_developer's_guide/js_modules/webpack.config.js
+++ stephen_grider/webpack2_the_complete_developer's_guide/js_modules/webpack.config.js
@@ -22,6 +22,15 @@ const config = {
           loader: 'css-loader'
         }),
         test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          // if img > 40000 bytes，將檔案儲存到某個目錄
+          // img <= 40000 bytes，加 img 寫到 bundle.js 中
+          { loader: 'url-loader', options: { limit: 40000 } },
+          'image-webpack-loader'
+        ]
       }
     ]
   }, // 將所有由 css-loader 載入的 css，寫入到 style.css 檔案中
