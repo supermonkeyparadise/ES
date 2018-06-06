2b68b0ab91c7156696c3ec9d752075a333eb215a
Unknown <steven.chou@uniqestate.com>
2018-06-06 12:42:36 +0800

    effect the code splitting

--- stephen_grider/webpack2_the_complete_developer's_guide/real_world/webpack.config.js
+++ stephen_grider/webpack2_the_complete_developer's_guide/real_world/webpack.config.js
@@ -1,11 +1,28 @@
 var webpack = require('webpack');
 var path = require('path');
 
+// 實際 package.json 內的 libs，但不一定要包含全部，依實際情況而定
+const VENDOR_LIBS = [
+  'faker',
+  'lodash',
+  'redux',
+  'react-redux',
+  'react-dom',
+  'react-input-range',
+  'redux-form',
+  'redux-thunk',
+  'react-router',
+  'react'
+];
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js', // bundle.js
+    vendor: VENDOR_LIBS // vendor.js
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js' // 依據 entry 內的 key 值，動態產生的檔名
   },
   module: {
     rules: [
@@ -19,5 +36,10 @@ module.exports = {
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor' // bundle.js 與 vendor.js 內有重複的 lib 都移到 vendor.js
+    })
+  ]
 };
